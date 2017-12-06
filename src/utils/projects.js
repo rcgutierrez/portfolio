54780959d45ef03d2142ed77900db644c46f1c6e
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-12-06 14:57:35 -0600

    typos corrected

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -14,7 +14,7 @@ const projects = [
   [
     "Kollaboscope",
     <a href="https://github.com/rcgutierrez/project-01" target="_blank" rel="noopener noreferrer"><img src={kollaboscope} alt="Kollaboscope Front Page" /></a>,
-    "Kollaboscope aims to facilliate building playlist collaboratively in a public setting; it's the jukebox for the 21st century. Users go to a 'room', designated from the URL path, and add songs from Youtube. These songs are then played back in order added.",
+    "Kollaboscope aims to facilitate building playlist collaboratively in a public setting; it's the jukebox for the 21st century. Users go to a 'room', designated from the URL path, and add songs from Youtube. These songs are then played back in order added.",
     "This project is in the process of being refactored into a React.js app and its backend has moved databases. We hope to build up its functionality to include the ability to add music from other streaming services.",
     false
   ],
