51274077eca9d61d3212b5bcabd918758308f757
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-12-07 08:56:38 -0600

    omitted lowercase

--- src/utils/entries.js
+++ src/utils/entries.js
@@ -3,16 +3,16 @@ const entries = [
     "General Assembly Web Development Immersive",
     "Student",
     "09.2017-12.2017",
-    "i attended this full-time course to change my life. This 9-5 class schedule for 3 months was a grind, but it was also a rewarding and enriching experience. We went over topics ranging starting from front languages, version control management, frameworks, object-oriented programming, setting up api calls, and more.",
-    "skills learned: react.js, node.js, mongoose, git, ajax, group teamwork",
+    "I attended this full-time course to change my life. This 9-5 class schedule for 3 months was a grind, but it was also a rewarding and enriching experience. We went over topics ranging starting from front languages, version control management, frameworks, object-oriented programming, setting up api calls, and more.",
+    "Skills learned: react.js, node.js, mongoose, git, ajax, group teamwork",
     "Austin, TX"
   ],
   [
     "The Boon Group",
     "Account Specialist",
     "10.2016-09.2017",
-    "In this role, i allocated medical coverage to employees according to their hours worked to keep my clients legally compliant. Using excel, I learned to manage these benefits for thousands of employees every month.",
-    "skills learned: advanced excel formulas, v-lookup, pivot tables, jira, long-term project management",
+    "In this role, I allocated medical coverage to employees according to their hours worked to keep my clients legally compliant. Using excel, I learned to manage these benefits for thousands of employees every month.",
+    "Skills learned: advanced excel formulas, v-lookup, pivot tables, jira, long-term project management",
     "Austin, TX"
   ],
   [
@@ -20,7 +20,7 @@ const entries = [
     "A/R Coordinator | A/P Support | Billing Specialist",
     "12.2014-09.2016",
     "As Accounts Receivable Coordinator, I handled the incoming remittences for a nationwide company that provided independent medical examinations. These payments had to be applied to the correct invoices and subsequently, as the Accounts Payable Support, I sent payments to the medical providers for their services.",
-    "skills learned: microsoft outlook, microsoft exel, microsoft access, microsoft cdn",
+    "Skills learned: microsoft outlook, microsoft exel, microsoft access, microsoft cdn",
     "Seattle, WA"
   ],
   [
